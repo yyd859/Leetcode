ecb63f92b27f702ae8f55b3f312079133439e541
yyd859 <49440702+yyd859@users.noreply.github.com>
2025-12-08 11:54:36 -0500

    Time: 6 ms (66.61%) | Memory: 17.8 MB (63.19%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -12,9 +12,16 @@ class Solution:
         # rev_x = x[::-1]
         # return  True if x==rev_x else False
     # or
-        x=str(x)
-        list_f=[i for i in x]
-        list_b=list_f.copy()
-        list_b.reverse()
-        return  True if list_f==list_b else False
-    # still not the best solution but good enough
+        # x=str(x)
+        # list_f=[i for i in x]
+        # list_b=list_f.copy()
+        # list_b.reverse()
+        # return  True if list_f==list_b else False
+    # still not the best solution but good enough, the best way with O(1) is
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        rev = 0
+        while x > rev:
+            rev = rev * 10 + x % 10
+            x //= 10
+        return x == rev or x == rev // 10
