db96648b8bb5debdef2176dba6e20e4191dc2731
yyd859 <49440702+yyd859@users.noreply.github.com>
2025-12-13 16:57:24 -0500

    Time: 2 ms (52.97%) | Memory: 19 MB (48.42%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,12 +1,21 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         # first try, dict.items() is for key and values, enumerate is for position and keys, (i in dict) is for keys only
-        dict={}
-        position=0
-        for num in nums:
-            if num not in dict:
-                dict[num]=position
-                position=position+1
-        for key,value in dict.items():
-            nums[value]=key
-        return position
+        # dict={}
+        # position=0
+        # for num in nums:
+        #     if num not in dict:
+        #         dict[num]=position
+        #         position=position+1
+        # for key,value in dict.items():
+        #     nums[value]=key
+        # return position
+        # second try: two pointers after hint from gpt
+        if not nums:
+            return 0
+        slow = 1  # 下一个写入位置
+        for fast in range(1, len(nums)):
+            if nums[fast] != nums[slow - 1]:
+                nums[slow] = nums[fast]
+                slow += 1
+        return slow
