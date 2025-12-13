246d083834574e89a6299965bc30341a8ec361cb
yyd859 <49440702+yyd859@users.noreply.github.com>
2025-12-13 16:55:28 -0500

    Time: 0 ms (100.00%) | Memory: 18.8 MB (74.10%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,12 +1,12 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        # first try, dict.items() is for key and values, enumerate is for position and keys, (i in dict) is for keys only
         dict={}
         position=0
         for num in nums:
             if num not in dict:
                 dict[num]=position
                 position=position+1
-        nums[:]=[0]*len(nums)
         for key,value in dict.items():
             nums[value]=key
         return position
