b8ab855b5b8323d777f2cbac8231c52b020d7e49
yyd859 <49440702+yyd859@users.noreply.github.com>
2025-12-16 22:40:47 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (76.47%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,7 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # first try
-        dict={}
-        for i in nums:
-            dict[i]=dict.get(i,0)+1
-        return min(dict,key=dict.get)
+        # dict={}
+        # for i in nums:
+        #     dict[i]=dict.get(i,0)+1
+        # return min(dict,key=dict.get)
+        # optimal solution
+        res = 0
+        for n in nums:
+            res ^= n
+        return res
