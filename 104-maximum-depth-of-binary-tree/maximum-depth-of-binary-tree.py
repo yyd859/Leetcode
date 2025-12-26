e185fb70c3fd352885788bf70d7868efa970e685
yyd859 <49440702+yyd859@users.noreply.github.com>
2025-12-26 14:25:53 -0500

    Time: 0 ms (100.00%) | Memory: 18.4 MB (92.49%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -4,12 +4,31 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         # DFS
         # the current node's depth is 1+max(left,right)
         # recursion
+        # if not root:
+        #     return 0
+        # return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
+        # BFS
+        # give a deque, everytime when you go one depth lower, treverse all the nodes for the next layer, until the next layer is all null
         if not root:
             return 0
-        return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
-        #
+        depth = 0
+        q=deque()
+        q.append(root)
+        while q:
+            depth +=1
+            for _ in range(len(q)): # execute just once, even the q is later appended with more nodes
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+        return depth
+
+
