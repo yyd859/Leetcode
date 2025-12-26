e8670debafec7e948a89f0277e8f03d86a4b8303
yyd859 <49440702+yyd859@users.noreply.github.com>
2025-12-26 15:18:01 -0500

    Time: 17 ms (11.27%) | Memory: 17.3 MB (96.06%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,27 +1,8 @@
 class Solution:
-    #First try,success
     def isPalindrome(self, x: int) -> bool:
-        # x=str(x)
-        # list_f=[i for i in x]
-        # list_b=[]
-        # for i in list_f:
-        #     list_b.insert(0,i)
-        # return  True if list_f==list_b else False
-    # hint: s[::-1] is the reversal of  list/string
-        # x =str(x)
-        # rev_x = x[::-1]
-        # return  True if x==rev_x else False
-    # or
-        # x=str(x)
-        # list_f=[i for i in x]
-        # list_b=list_f.copy()
-        # list_b.reverse()
-        # return  True if list_f==list_b else False
-    # still not the best solution but good enough, the best way with O(1) is
-        if x < 0 or (x % 10 == 0 and x != 0):
-            return False
-        rev = 0
-        while x > rev:
-            rev = rev * 10 + x % 10
-            x //= 10
-        return x == rev or x == rev // 10
+        x=list(str(x))
+        l=len(x)
+        for i in range(0,l//2+1):
+            if x[i]!=x[l-i-1]:
+                return False
+        return True
